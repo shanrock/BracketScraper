5e3d23d6798c9a72a37c88068c3b7846be00ee66
shanrock <slowry430@gmail.com>
2019-04-17 08:09:39 -0500

    Update BracketScraper.py

--- BracketScraper/BracketScraper.py
+++ BracketScraper/BracketScraper.py
@@ -4,32 +4,49 @@ import requests
 import itertools as it
 import re
 from dateutil.parser import parse
+import datetime as dt
 
-def scrape_data(url):
+def scrape_data(url,game_id):
     url = '{}{}'.format(base_url,day)
-    response = requests.get(url)
-    soupy = soup(response.content, 'lxml')     
+    r = requests.get(url)
+    soupy = soup(r.content, 'lxml')    
+  
     # connecting to database server
-    server = 'rateurrefserver.database.windows.net' 
+    server = '#.database.windows.net' 
     database = '#' 
     dbUsername = '#' 
-    password = '#' 
+    password = '#'  
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+dbUsername+';PWD='+ password )
     cursor = cnxn.cursor()
     
-    containers = soupy.findAll("div",{"class" : "team-container"})
+#    containers = soupy.findAll("div",{"class" : "team-container"})
+    containers = soupy.findAll("div",{"class" : "competitors"})
     for container in containers:
-        spans = container.findAll("span")
-        divs = container.find("div",{"class": "record"})
-        ranks = spans[0].text
-        team_name = spans[1].text
-        team_mascot = spans[2].text
-        team_abbr = spans[3].text
-        team_record = divs.text
+        info_wrapper = soupy.findAll("div", {"class": "team-info-wrapper"})
+        team_away = info_wrapper[0].find("span", {"class":"long-name"}).text
+        team_away_mascot = info_wrapper[0].find("span", {"class":"short-name"}).text
+        team_away_abbr = info_wrapper[0].find("span", {"class":"abbrev"}).text
+        team_home = info_wrapper[1].find("span", {"class":"long-name"}).text
+        team_home_mascot = info_wrapper[1].find("span", {"class":"short-name"}).text
+        team_home_abbr = info_wrapper[1].find("span", {"class":"abbrev"}).text
+        game_detail = soupy.find("div", {"class" : "game-details"}).text
+        
+        # grabbing the round information
+        if(game_id == 1 or game_id == 2 or game_id == 3):
+            game_round = game_detail.split(" - ")[1]
+        else:
+            game_round = game_detail.split(" - ")[2]
+        
+        # converting the data-date to correct datetime for db
+        # grabbing the attribute value is faster than using selenium
+        # storing in utc time to easily connect game to tweet data
+        
         timing = soupy.select_one('[data-date]')['data-date']
         game_time = parse(timing).time()
         game_date = parse(timing).date()
-
+        date_time = dt.datetime.combine(game_date, game_time)
+        
+        # refs were left off from page 403
         try:
             refs_container = soupy.find("div", {"class" : "game-info-note__container"})
             refs = refs_container.text
@@ -43,26 +60,30 @@ def scrape_data(url):
         except AttributeError:
             ref1 = "John Gattney"
             ref2 = "Earl Walton" 
-            ref3 = "Paul Faia"
-        
+            ref3 = "Paul Faia"   
+            
+        print(game_id)
         # sql query to insert date into the database
-        games_query = "INSERT INTO [dbo].[bracket] (name, abbr, mascot, team_record, ranks, game_time, game_date, ref1,ref2, ref3) VALUES (?,?,?,?,?,?,?,?,?,?)"
-        cursor.execute(games_query, (team_name, team_abbr, team_mascot, team_record, ranks, game_time, game_date, ref1,ref2, ref3))
-
+        games_query = "INSERT INTO [dbo].[test_bracket] (id,game_round,home_team,home_mascot,\
+        home_abbr,away_team,away_mascot,away_abbr,date_time,ref1,ref2,ref3) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)"
+        cursor.execute(games_query, (game_id, game_round, team_home, team_home_mascot, team_home_abbr,
+                                     team_away, team_away_mascot, team_away_abbr, date_time, ref1,ref2, ref3))
+     
     # closing the connection
     cnxn.commit()
     cursor.close()
     del cursor
     cnxn.close()
     
-    
+  
 if __name__ == '__main__':
 
    base_url ="http://www.espn.com/mens-college-basketball/game/_/id/401123"
-    
-   #Every page needed for 
+   game_id = 0
+    # loop through every page ( except first four ) on bracket page
    for day in it.chain(range(374,377), range(381,441)):
-       scrape_data(base_url)
+       game_id += 1
+       scrape_data(base_url,game_id)
 
 
 
